ca314013c7fb0a087adfad396e0a1640c0f10b3a
friedpotato1 <126858368+friedpotato1@users.noreply.github.com>
2023-11-03 23:44:08 +0300

    Add files via upload

---
+++ fifa.py
@@ -0,0 +1,12 @@
+winner = str(input('Which player won > '))
+winner = winner.lower()
+wins = {}
+with open('winners.txt','a') as win:
+    win.write(f'{winner}\n')
+with open ('winners.txt', 'r') as f:
+    for stat in f:
+        stat = stat.strip()
+        wins.setdefault(stat,0)
+        wins[stat] = wins[stat] + 1
+print('\n',"Here are the detailed wins stats :\n")
+print('\t',wins)
